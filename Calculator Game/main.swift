8786bfb861ec258f0296a2df36468655bc525ba1
DeadMansMarch <lsp425@gmail.com>
2017-07-18 00:25:37 -0400

    Initial Commit

---
+++ Calculator Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calculator Game
+//
+//  Created by Liam Pierce on 7/18/17.
+//  Copyright © 2017 Liam Pierce. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
